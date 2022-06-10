46367cf3695e5e7de03899f14065cea6d1886b12
Kubra Bosnak <skubrab@gmail.com>
2022-06-10 17:48:08 +0300

    ADO.NET SAMPLE WITH NORTHWIND DATABASE
    ADO.NET,  Connection Strings, DataAdapter, DataTable, DataRow, DataColumn, ExecuteNonQuery, ExecuteScalar, SqlDataReader ve ExecuteReader, DataGridView

---
+++ AdoNetSamples/AdoNetSamples/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace AdoNetSamples
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBaglan = new System.Windows.Forms.Button();
+            this.btnLoadCustomers = new System.Windows.Forms.Button();
+            this.dgvCustomers = new System.Windows.Forms.DataGridView();
+            this.btnLoadCustomers2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnInsertCustomer = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            this.txtCustomerID = new System.Windows.Forms.TextBox();
+            this.btnExecuteScalar = new System.Windows.Forms.Button();
+            this.cmbCustomers = new System.Windows.Forms.ComboBox();
+            this.btnFilter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBaglan
+            // 
+            this.btnBaglan.Location = new System.Drawing.Point(12, 12);
+            this.btnBaglan.Name = "btnBaglan";
+            this.btnBaglan.Size = new System.Drawing.Size(97, 23);
+            this.btnBaglan.TabIndex = 0;
+            this.btnBaglan.Text = "DB\'ye Bağlan";
+            this.btnBaglan.UseVisualStyleBackColor = true;
+            this.btnBaglan.Click += new System.EventHandler(this.btnBaglan_Click);
+            // 
+            // btnLoadCustomers
+            // 
+            this.btnLoadCustomers.Location = new System.Drawing.Point(115, 12);
+            this.btnLoadCustomers.Name = "btnLoadCustomers";
+            this.btnLoadCustomers.Size = new System.Drawing.Size(114, 23);
+            this.btnLoadCustomers.TabIndex = 1;
+            this.btnLoadCustomers.Text = "Müşterileri Getir";
+            this.btnLoadCustomers.UseVisualStyleBackColor = true;
+            this.btnLoadCustomers.Click += new System.EventHandler(this.btnLoadCustomers_Click);
+            // 
+            // dgvCustomers
+            // 
+            this.dgvCustomers.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomers.Location = new System.Drawing.Point(12, 41);
+            this.dgvCustomers.Name = "dgvCustomers";
+            this.dgvCustomers.RowTemplate.Height = 25;
+            this.dgvCustomers.Size = new System.Drawing.Size(465, 260);
+            this.dgvCustomers.TabIndex = 2;
+            // 
+            // btnLoadCustomers2
+            // 
+            this.btnLoadCustomers2.Location = new System.Drawing.Point(235, 12);
+            this.btnLoadCustomers2.Name = "btnLoadCustomers2";
+            this.btnLoadCustomers2.Size = new System.Drawing.Size(114, 23);
+            this.btnLoadCustomers2.TabIndex = 3;
+            this.btnLoadCustomers2.Text = "Müşterileri Getir 2";
+            this.btnLoadCustomers2.UseVisualStyleBackColor = true;
+            this.btnLoadCustomers2.Click += new System.EventHandler(this.btnLoadCustomers2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnDelete);
+            this.groupBox1.Controls.Add(this.btnUpdate);
+            this.groupBox1.Controls.Add(this.btnInsertCustomer);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtCompanyName);
+            this.groupBox1.Controls.Add(this.txtCustomerID);
+            this.groupBox1.Location = new System.Drawing.Point(484, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(238, 260);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "EKLE / GÜNCELLE / SİL";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(157, 159);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "SİL";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(157, 130);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "GÜNCELLE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnInsertCustomer
+            // 
+            this.btnInsertCustomer.Location = new System.Drawing.Point(157, 101);
+            this.btnInsertCustomer.Name = "btnInsertCustomer";
+            this.btnInsertCustomer.Size = new System.Drawing.Size(75, 23);
+            this.btnInsertCustomer.TabIndex = 2;
+            this.btnInsertCustomer.Text = "EKLE";
+            this.btnInsertCustomer.UseVisualStyleBackColor = true;
+            this.btnInsertCustomer.Click += new System.EventHandler(this.btnInsertCustomer_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Company Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Customer ID";
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.Location = new System.Drawing.Point(121, 72);
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.Size = new System.Drawing.Size(111, 23);
+            this.txtCompanyName.TabIndex = 0;
+            // 
+            // txtCustomerID
+            // 
+            this.txtCustomerID.Location = new System.Drawing.Point(121, 43);
+            this.txtCustomerID.Name = "txtCustomerID";
+            this.txtCustomerID.Size = new System.Drawing.Size(111, 23);
+            this.txtCustomerID.TabIndex = 0;
+            // 
+            // btnExecuteScalar
+            // 
+            this.btnExecuteScalar.Location = new System.Drawing.Point(364, 12);
+            this.btnExecuteScalar.Name = "btnExecuteScalar";
+            this.btnExecuteScalar.Size = new System.Drawing.Size(113, 23);
+            this.btnExecuteScalar.TabIndex = 5;
+            this.btnExecuteScalar.Text = "Toplam Müşteri";
+            this.btnExecuteScalar.UseVisualStyleBackColor = true;
+            this.btnExecuteScalar.Click += new System.EventHandler(this.btnExecuteScalar_Click);
+            // 
+            // cmbCustomers
+            // 
+            this.cmbCustomers.FormattingEnabled = true;
+            this.cmbCustomers.Location = new System.Drawing.Point(576, 12);
+            this.cmbCustomers.Name = "cmbCustomers";
+            this.cmbCustomers.Size = new System.Drawing.Size(146, 23);
+            this.cmbCustomers.TabIndex = 6;
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(510, 12);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(54, 23);
+            this.btnFilter.TabIndex = 7;
+            this.btnFilter.Text = "BUL";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 313);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.cmbCustomers);
+            this.Controls.Add(this.btnExecuteScalar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnLoadCustomers2);
+            this.Controls.Add(this.dgvCustomers);
+            this.Controls.Add(this.btnLoadCustomers);
+            this.Controls.Add(this.btnBaglan);
+            this.Name = "Form1";
+            this.Text = "ADONET İŞLEMLERİ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBaglan;
+        private System.Windows.Forms.Button btnLoadCustomers;
+        private System.Windows.Forms.DataGridView dgvCustomers;
+        private System.Windows.Forms.Button btnLoadCustomers2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnInsertCustomer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCompanyName;
+        private System.Windows.Forms.TextBox txtCustomerID;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnExecuteScalar;
+        private System.Windows.Forms.ComboBox cmbCustomers;
+        private System.Windows.Forms.Button btnFilter;
+    }
+}
+
---
+++ AdoNetSamples/AdoNetSamples/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace AdoNetSamples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // localdb ye sağ tıklayıp property den connectionn stringi çekebiliriz.
+
+        // Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
+
+        private string connstr = "Server=(localdb)\\MSSQLLocalDB; Database=Northwind;Trusted_Connection=True";
+
+
+        private void btnBaglan_Click(object sender, EventArgs e)
+        {
+            // SQL e kullanıcı adı ve şifre ile bağlanmak istersek ;
+            // Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;
+
+            // Windows Authentication ile bağlanmak istersek;
+            // Server=myServerAddress;Database=myDataBase;Trusted_Connection=True;
+
+
+            // 1. BAĞLANMA YÖNTEMİ
+
+            //SqlConnection connection = new SqlConnection();
+            //connection.ConnectionString = connstr;
+
+            // 2. BAĞLANMA YÖNTEMİ
+            SqlConnection connection = new SqlConnection(connstr);
+
+            connection.Open();
+            MessageBox.Show("Bağlantı Açıldı.");
+
+            connection.Close();
+            MessageBox.Show("Bağlantı Kapandı");
+        }
+
+        private void btnLoadCustomers_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(connstr);   // Bağlantı nesnemizi oluşturduk
+
+            string query = "SELECT * FROM Customers";
+
+            // 1. YÖNTEM
+
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            // 2. YÖNTEM
+
+            //SqlCommand cmd = new SqlCommand();
+            //cmd.CommandText = query;
+            //cmd.Connection = conn;
+
+            // conn açar sorguyu çalıştırır ve conn kapatır.
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);  // sql data adaptörünü oluşturduk
+            DataTable table = new DataTable();   // dgv için tablo oluşturduk
+
+            adapter.Fill(table);  // tablonun için fill methodu ile doldurduk
+
+            dgvCustomers.DataSource = table;  //dgv ye yazdırdık
+        }
+
+        private void btnLoadCustomers2_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(connstr);
+            string query = "SELECT CustomerID, CompanyName, ContactName FROM Customers";
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();  // data okuma işlemi yapılır
+
+            DataTable table = new DataTable();
+            table.Columns.Add("CustomerID", typeof(string));
+            table.Columns.Add("CompanyName", typeof(string));
+            table.Columns.Add("ContactName", typeof(string));
+
+            while (reader.Read())
+            {
+                DataRow row = table.NewRow();
+                row["CustomerID"] = reader["CustomerID"];
+                row["CompanyName"] = reader["CompanyName"];
+                row["ContactName"] = reader["ContactName"];
+
+                table.Rows.Add(row);
+
+                //if (row["CustomerID"].ToString().StartsWith("B"))   // sadece B harfi ile başlayanları getirir.
+                //{
+                //table.Rows.Add(row);
+                //}
+
+            }
+
+            reader.Close();
+            conn.Close();
+
+            //reader.DisposeAsync();  // ram'den yok etmek için
+            //conn.Dispose();
+
+            dgvCustomers.DataSource = table;
+
+        }
+
+        private void btnInsertCustomer_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(connstr);
+
+            string custId = txtCustomerID.Text;
+            string compName = txtCompanyName.Text;
+
+            //TEK TIRNAKLA YAZMAK GÜVENLİK AÇIĞIDIR.
+
+            //string query = $"INSERT INTO [dbo].[Customers] ([CustomerID] ,[CompanyName] ,[ContactName] ,[ContactTitle] ,[Address] ,[City] ,[Region] ,[PostalCode] ,[Country] ,[Phone] ,[Fax]) VALUES ('{custId}' ,'{compName}' ,NULL ,NULL ,NULL ,NULL ,NULL ,NULL ,NULL ,NULL ,NULL)";
+
+            string query = $"INSERT INTO [dbo].[Customers] ([CustomerID] ,[CompanyName] ,[ContactName] ,[ContactTitle] ,[Address] ,[City] ,[Region] ,[PostalCode] ,[Country] ,[Phone] ,[Fax]) VALUES (@CustomerID, @CompanyName ,NULL ,NULL ,NULL ,NULL ,NULL ,NULL ,NULL ,NULL ,NULL)";
+
+
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@CustomerID", custId);
+            cmd.Parameters.AddWithValue("@CompanyName", compName);
+
+            conn.Open();
+            int count = cmd.ExecuteNonQuery(); // etkilenen satır sayısını döner. Insert / update / delete sorguları için kullanılır
+            conn.Close();
+
+            btnLoadCustomers_Click(btnLoadCustomers, EventArgs.Empty);
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection conn = new SqlConnection(connstr);
+
+            string custId = txtCustomerID.Text;
+            string compName = txtCompanyName.Text;
+
+            string query = $"UPDATE Customers SET CompanyName = @CompanyName WHERE CustomerID= @CustomerID";
+
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@CustomerID", custId);
+            cmd.Parameters.AddWithValue("@CompanyName", compName);
+
+            conn.Open();
+            int count = cmd.ExecuteNonQuery();
+            conn.Close();
+
+            btnLoadCustomers_Click(btnLoadCustomers, EventArgs.Empty);
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(connstr);
+            string custId = txtCustomerID.Text;
+
+            string query = $"DELETE FROM Customers WHERE CustomerID=@CustomerID";
+
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@CustomerID", custId);
+
+            conn.Open();
+            int count = cmd.ExecuteNonQuery();
+            conn.Close();
+
+            btnLoadCustomers_Click(btnLoadCustomers, EventArgs.Empty);
+        }
+
+        private void btnExecuteScalar_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(connstr);
+            string custId = txtCustomerID.Text;
+
+            string query = $"SELECT COUNT(*) AS [COUNT] FROM Customers";
+
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            conn.Open();
+
+            object result = cmd.ExecuteScalar();
+            int count = (int)result;
+
+            conn.Close();
+
+            MessageBox.Show("Toplam Müşteri : " + count);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<Customer> customers = new List<Customer>();
+
+            SqlConnection conn = new SqlConnection(connstr);
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT CustomerID, CompanyName, ContactTitle FROM Customers";
+
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Customer customer = new Customer()
+                    {
+                        CustomerID = (string)reader["CustomerID"],
+                        CompanyName = reader.GetFieldValue<string>("CompanyName"),
+                        ContactTitle = reader.IsDBNull("ContactTitle") ? null : reader.GetFieldValue<string>("ContactTitle")
+                    };
+                    customers.Add(customer);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Hata Oluştu!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (conn.State != ConnectionState.Closed && conn.State != ConnectionState.Broken)
+                {
+                    conn.Close();
+                }
+            }
+
+            cmbCustomers.DisplayMember = "CompanyName";
+            cmbCustomers.ValueMember = "CustomerID";
+            cmbCustomers.DataSource = customers;
+        }
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            if (cmbCustomers.SelectedIndex == -1)
+            {
+                MessageBox.Show("Müşteri seçmediniz..");
+                return;
+            }
+
+            SqlConnection conn = new SqlConnection(connstr);
+
+            string query = "SELECT * FROM Customers WHERE CustomerID = @CustomerID";
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            //Customer selected = cmbCustomers.SelectedItem as Customer;
+            //cmd.Parameters.AddWithValue("@CustomerID", selected.CustomerID);
+
+            cmd.Parameters.AddWithValue("@CustomerID", cmbCustomers.SelectedValue);
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable table = new DataTable();
+
+            adapter.Fill(table);
+
+            dgvCustomers.DataSource = table;
+
+
+
+
+        }
+    }
+}
